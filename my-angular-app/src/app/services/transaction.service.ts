840a7c0bbc9c53a1de2396a665cad9d4a02d4a22
Hissam Salahuddin <hissam@pakainvest.de>
2025-09-01 21:53:21 +0200

    Feat: add frontend Angular app

--- my-angular-app/src/app/services/transaction.service.ts
+++ my-angular-app/src/app/services/transaction.service.ts
@@ -17,5 +17,41 @@ getTransactions() {
   );
   }
 
+ getTransactionDetails(transactionId: string) {
+  return this.http.get(this.baseUrl + `transactions/${transactionId}`).pipe(
+    catchError(err => {
+      console.error('Error fetching transaction details', err);
+      return throwError(() => new Error('Error fetching transaction details'));
+    })
+  );
+  }
+
+  addTransaction(transactionData: any) {
+  return this.http.post(this.baseUrl + 'transactions', transactionData).pipe(
+    catchError(err => {
+      console.error('Error adding transaction', err);
+      return throwError(() => new Error('Error adding transaction'));
+    })
+  );
+  }
+
+  updateTransaction(transactionId: string, transactionData: any) {
+  return this.http.put(this.baseUrl + `transactions/${transactionId}`, transactionData).pipe(
+    catchError(err => {
+      console.error('Error updating transaction', err);
+      return throwError(() => new Error('Error updating transaction'));
+    })
+  );
+  }
+
+  deleteTransaction(transactionId: string) {
+  return this.http.delete(this.baseUrl + `transactions/${transactionId}`).pipe(
+    catchError(err => {
+      console.error('Error deleting transaction', err);
+      return throwError(() => new Error('Error deleting transaction'));
+    })
+  );
+  }
+
 
 }
